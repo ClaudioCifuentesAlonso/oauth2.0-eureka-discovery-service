635015a620c130c2dec5685b660625fae1c111c9
Claudio Cifuentes <cacifuentesalonso@gmail.com>
2021-06-24 17:55:20 -0500

    Application code

---
+++ src/test/java/com/claudio/oauth2/eureka/Oauth2EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudio.oauth2.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
